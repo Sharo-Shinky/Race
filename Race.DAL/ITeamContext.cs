a262c5439ebf23da0c03a2f19017dda64dcfa237
Sharo <sharo-shinky@hotmail.com>
2019-05-07 13:43:12 +0200

    Begonnen aan verwerken van Repository patern workshop

--- Race.DAL/ITeamContext.cs
+++ Race.DAL/ITeamContext.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Race.DAL.Interface;
 
 namespace Race.DAL
 {
-    interface ITeamContext
+    public interface ITeamContext
     {
+        void Add(TeamStruct teamStruct);
+        void Remove(int id);
+        List<TeamStruct> GetAll();
+        void Update(TeamStruct teamStruct);
     }
 }
